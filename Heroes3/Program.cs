9af8c4e05e7caa65fbce99a30aed8fa416539e5f
wesek <wesek@DZARAII>
2022-03-04 20:30:08 +0300

    Добавьте файлы проекта.

---
+++ Heroes3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heroes3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Spearman spearman1 = new Spearman();
+            Skeleton skeleton1 = new Skeleton();
+            int damage1, damage2;
+            while (!spearman1.IsDead() && !skeleton1.IsDead())
+            {
+                damage1 = spearman1.Storming();
+                skeleton1.LossOfHealth(damage1);
+                if (!skeleton1.IsDead())
+                {
+                    damage2 = skeleton1.Storming();
+                    spearman1.LossOfHealth(damage2);
+                    Console.WriteLine(damage2);
+                }
+            }
+          
+            Console.WriteLine("Копейщик 1 мертв: {0}", spearman1.IsDead());
+            Console.WriteLine("Скелетон 1 мертв: {0}", skeleton1.IsDead());
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Heroes3/Spearman.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heroes3
+{
+    internal class Spearman
+    {
+        private int attack = 1;
+        private int defends = 5;
+        private int damage = 1;
+        private int health = 10;
+        private int speed = 4;
+
+
+
+        /// <summary>
+        /// Атаковать
+        /// </summary>
+        /// <returns>Сила атаки</returns>
+        public int Storming()
+        {
+            //Создание объекта для генерации чисел
+            Random rnd = new Random((int)(DateTime.Now.Ticks));
+            //Random rnd = new Random();
+
+            //Получить случайное число (в диапазоне от 1 до 3)
+            int value = rnd.Next(1, 3);
+
+            return value;
+        }
+        public void LossOfHealth(int damage)
+        {
+            health = health - damage;
+        }
+        public bool IsDead()
+        {
+            bool isDead;
+            if (health > 0)
+            {
+                isDead = false;
+            }
+            else
+            {
+                isDead = true;
+            }
+            return isDead;
+        }
+    }
+}
